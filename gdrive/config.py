e3fadc9a44610c1e417aa23b57052eb2b2b5a4eb
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-06-09 14:20:35 -0300

    Update config.py

--- gdrive/config.py
+++ gdrive/config.py
@@ -10,9 +10,9 @@ GDRIVE_SHEETS_ID = "1Sw5EfdPIgAsPddCoHY-bO-91JyWAZpK4xW7FjZIz4Vw"
 
 
 # Nome das abas na planilha
-LIFTING_SHEET_NAME = "asos"
-CRANE_SHEET_NAME = "treinamentos"
-EMPLOYEE_SHEET_NAME = "empresas"
+ASO_SHEET_NAME = "asos"  # Aba para ASOs
+EMPLOYEE_SHEET_NAME = "empresas"  # Aba para empresas
+EMPLOYEE_DATA_SHEET_NAME = "funcionarios"  # Aba para funcionários
 
 def get_credentials_dict():
     """Retorna as credenciais do serviço do Google, seja do arquivo local ou do Streamlit Cloud."""
@@ -44,3 +44,4 @@ def get_credentials_dict():
         except Exception as e:
             st.error(f"Erro ao carregar credenciais do arquivo local: {str(e)}")
             raise e
+
