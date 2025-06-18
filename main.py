3d15e6b669549160a712fcfea10a50fd7f149fd8
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-06-18 07:26:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,30 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 if root_dir not in sys.path:
     sys.path.append(root_dir)
 
-def configurar_pagina():
-    st.set_page_config(
-        page_title="SSO AI",
-        page_icon="🛡️",
-        layout="wide",
-        initial_sidebar_state="auto",
-        menu_items={
-            'Get Help': 'https://www.streamlit.io/community',
-            'Report a bug': "mailto:cristianfc2015@hotmail.com",
-            'About': """
-            ## SSO AI
-            Sistema de Análise Inteligente de Documentos de SSO.
-            Versão 1.0.1
-            """
-        }
-    )
+
+from auth.login_page import show_login_page, is_user_logged_in
 
 
-configurar_pagina()
+st.set_page_config(
+    page_title="SSO AI",
+    page_icon="🛡️",
+    layout="wide",
+    initial_sidebar_state="auto", # A sidebar será exibida
+    menu_items={
+        'Get Help': 'https://www.streamlit.io/community',
+        'Report a bug': "mailto:cristianfc2015@hotmail.com",
+        'About': """
+        ## SSO AI
+        Sistema de Análise Inteligente de Documentos de SSO.
+        Versão 1.0.1
+        """
+    }
+)
 
-
-st.title("Bem-vindo ao SSO AI 🛡️")
-st.write("Navegue pelas páginas na barra lateral para começar.")
-st.info("O login é necessário para acessar as funcionalidades.")
-
+if is_user_logged_in():
+   
+    st.switch_page("pages/1_Visão_Geral.py")
+else:
+    show_login_page()
 
 
