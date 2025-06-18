99b53d0125da5cfea2a35014b5fb70e3ea160551
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-06-18 08:04:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import streamlit as st
 import sys
 import os
 
-
+# Adiciona o diretório raiz ao PYTHONPATH
 root_dir = os.path.dirname(os.path.abspath(__file__))
 if root_dir not in sys.path:
     sys.path.append(root_dir)
 
-
 from auth.login_page import show_login_page, is_user_logged_in
 
-
+# Configuração da página
 st.set_page_config(
     page_title="SSO AI",
     page_icon="🛡️",
     layout="wide",
-    initial_sidebar_state="auto", # A sidebar será exibida
+    initial_sidebar_state="auto",
     menu_items={
         'Get Help': 'https://www.streamlit.io/community',
         'Report a bug': "mailto:cristianfc2015@hotmail.com",
@@ -27,10 +26,20 @@ st.set_page_config(
     }
 )
 
+
+hide_main_nav_link = """
+    <style>
+    a[href="/"] {
+        display: none;
+    }
+    </style>
+"""
+st.markdown(hide_main_nav_link, unsafe_allow_html=True)
+
+
+
 if is_user_logged_in():
-   
     st.switch_page("pages/1_Visão_Geral.py")
 else:
     show_login_page()
-
 
