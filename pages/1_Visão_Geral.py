c930e490f41757ac7f6890d53312b4e3c380ae20
CRISTIAN CARLOS <140450586+CRIZANTE1@users.noreply.github.com>
2025-06-18 07:27:26 -0300

    Update 1_Visão_Geral.py

--- pages/1_Visão_Geral.py
+++ pages/1_Visão_Geral.py
@@ -2,20 +2,23 @@ import streamlit as st
 import sys
 import os
 
-
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if root_dir not in sys.path:
     sys.path.append(root_dir)
 
 from operations.front import front_page
-from auth.login_page import show_login_page, show_sidebar
+
+from auth.login_page import show_sidebar, is_user_logged_in
 
 def main():
-    
-    if show_login_page():
-        st.session_state.user = st.session_state.user if 'user' in st.session_state else None
-        show_sidebar()
-        front_page()
+    if not is_user_logged_in():
+        st.warning("Por favor, faça o login para acessar esta página.")
+        st.page_link("main.py", label="Ir para a página de Login", icon="🏠")
+        st.stop() 
+   
+    st.session_state.user = st.session_state.user if 'user' in st.session_state else None
+    show_sidebar()
+    front_page()
 
 if __name__ == "__main__":
     main()
